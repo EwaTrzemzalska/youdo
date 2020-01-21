0c85d18c467b60d5cc79319a075f414141fc1d31
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-21 14:08:44 +0100

    Add first functions conceptions

---
+++ src/youdo/state.clj
@@ -0,0 +1,10 @@
+(ns youdo.state)
+
+(defn list-tasks
+  "Given a file path, returns list of tasks."
+  [path])
+
+(defn add-task
+  "Given a file path, adds task to the end of the tasks list."
+  [path task]
+  )
