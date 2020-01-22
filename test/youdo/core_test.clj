dd0168b196ae748c0857c961d35948d2c7a420e5
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-22 21:54:50 +0100

    Add first test

--- test/youdo/core_test.clj
+++ test/youdo/core_test.clj
@@ -1,7 +1,16 @@
 (ns youdo.core-test
   (:require [clojure.test :refer :all]
-            [youdo.core :refer :all]))
+            [youdo.core :as core]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "First test"
+   (is (= 1 1))))
+
+(deftest proper-arguments-parsing
+  (is (= (core/parse-args "add"
+                          ["-t" "Clean" "-p" "./house.youdo"])
+         {:options {:task "Clean", :path "./house.youdo"}
+          :arguments [],
+          :summary  "  -t, --task TASK  new task      Task description\n  -p, --path PATH  default-path  Add path\n  -h, --help"
+          :errors nil})))
+
