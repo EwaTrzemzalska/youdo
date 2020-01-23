4aabbbb3b59e11c01408b92580401c461bb22cf5
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-23 13:59:06 +0100

    Add tests for incorrect arguments

--- test/youdo/core_test.clj
+++ test/youdo/core_test.clj
@@ -1,5 +1,5 @@
 (ns youdo.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [is testing deftest]]
             [youdo.core :as core]))
 
 (deftest proper-arguments-parsing
@@ -12,5 +12,20 @@
           :path "./house.youdo"})))
 
 (deftest incorrect-arguments-cause-an-error
-  (is (thrown-with-msg? Exception #"Incorrect action" 
-                        (core/parse-args "print" []))))
+  (testing "User provides incorrect action"
+    (is (thrown-with-msg? Exception #"Incorrect action"
+                          (core/parse-args "print" []))))
+  (testing "User doesn't provide an action"
+    (is (thrown-with-msg? Exception #"Provide an action"
+                          (core/parse-args "" []))))
+  (testing "User doesn't provide a path"
+    (is (thrown-with-msg? Exception #"Provide a path" 
+                          (core/parse-args "list" []))))
+  (testing "User doesn't provide a task" 
+    (is (thrown-with-msg? Exception #"Provide a task" 
+                          (core/parse-args 
+                           "add" ["-p" "./house.youdo"]))))
+  (testing "User provides a incorrect path"
+    (is (thrown-with-msg? Exception #"Provide a correct path" 
+                          (core/parse-args "add" ["-t " "Clean" "-p" "~Documents/check-weather.clj"])))))
+
