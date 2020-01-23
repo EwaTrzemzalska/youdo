9952980aac82c26fe4491363b04be5797d60a58d
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-23 17:54:04 +0100

    Change parse-args to passing happy path tests

--- src/youdo/core.clj
+++ src/youdo/core.clj
@@ -1,10 +1,24 @@
 (ns youdo.core
-  (:require [youdo.state :as state])
+  (:require [youdo.state :as state]
+            [clojure.tools.cli :as cli-tools])
   (:gen-class))
 
-(defn -main
-  [& args]
-  ;; dostajesz action, task i path
-  (comment (if (= action "list") ;; TODO: Change to cond (conditional)
-             (state/list-tasks path)
-             (state/add-task path task))))
+(def cli-options
+  ;; An option with a required argument
+  [["-t" "--task TASK" "Task description"]
+   ["-p" "--path PATH" "Add path"
+    :default "default-path"]
+   ;; A boolean option defaulting to nil
+   ["-h" "--help"]])
+
+(defn parse-args [action args]
+  (let [opts (cli-tools/parse-opts args cli-options)
+        task (get-in opts [:options :task])
+        path (get-in opts [:options :path])]
+    (cond-> {:action action
+             :path path}
+      task (assoc :task task))))
+
+(defn -main [action & args]
+  (println (parse-args action args)))
+
