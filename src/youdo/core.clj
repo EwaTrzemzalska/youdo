4d14989e0d2f70c1f1296e121627829c1b0b9013
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-24 16:45:28 +0100

    Refactore with when functions

--- src/youdo/core.clj
+++ src/youdo/core.clj
@@ -16,27 +16,21 @@
   (let [opts (cli-tools/parse-opts args cli-options)
         task (get-in opts [:options :task])]
     
-    (case action
-      "add" (case task 
-             nil (throw (ex-info "Task not found"
-                              {:some "data that helps debug problem, for example arguments that were passsed to the function"}))
-              (cond-> {:action (str/lower-case action)
-                       :path (get-in opts [:options :path])}
-                task (assoc :task task)))
-      "list" (cond-> {:action (str/lower-case action)
-                      :path (get-in opts [:options :path])}
-               task (assoc :task task))
-      "" (throw (ex-info "Action not found"
-                         {:some "data that helps debug problem, for example arguments that were passsed to the function"}))
+    (when (str/blank? action)
+      (throw (ex-info "Action not found"
+                      {:some "data that helps debug problem, for example arguments that were passsed to the function"})))
+    
+    (when (and (= action "add") (nil? task))
+      (throw (ex-info "Task not found"
+                      {:some "data that helps debug problem, for example arguments that were passsed to the function"})))
+    
+    (when-not (#{"add" "list"} action)
       (throw (ex-info "Incorrect action"
-                      {:some "data that helps debug problem, for example arguments that were passsed to the function"})))))
+                      {:some "data that helps debug problem, for example arguments that were passsed to the function"})))
+    
+    (cond-> {:action (str/lower-case action)
+             :path (get-in opts [:options :path])}
+      task (assoc :task task))))
 
 (defn -main [action & args]
   (println (parse-args action args)))
-
-; ((str/lower-case action)
- 
-;  (if (= action "")
-;    (throw (ex-info "Explain the error to the other developer. This is what will show up in the terminal, not to the user"
-;                    {:some "data that helps debug problem, for example arguments that were passsed to the function"})))
-;  )
