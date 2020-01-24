91c770bdcebed32afc00740b3e6a1ecfb036cfe2
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-24 16:12:08 +0100

    Add functionality which passes all tests

--- src/youdo/core.clj
+++ src/youdo/core.clj
@@ -1,6 +1,7 @@
 (ns youdo.core
   (:require [youdo.state :as state]
-            [clojure.tools.cli :as cli-tools])
+            [clojure.tools.cli :as cli-tools]
+            [clojure.string :as str])
   (:gen-class))
 
 (def cli-options
@@ -13,12 +14,29 @@
 
 (defn parse-args [action args]
   (let [opts (cli-tools/parse-opts args cli-options)
-        task (get-in opts [:options :task])
-        path (get-in opts [:options :path])]
-    (cond-> {:action action
-             :path path}
-      task (assoc :task task))))
+        task (get-in opts [:options :task])]
+    
+    (case action
+      "add" (case task 
+             nil (throw (ex-info "Task not found"
+                              {:some "data that helps debug problem, for example arguments that were passsed to the function"}))
+              (cond-> {:action (str/lower-case action)
+                       :path (get-in opts [:options :path])}
+                task (assoc :task task)))
+      "list" (cond-> {:action (str/lower-case action)
+                      :path (get-in opts [:options :path])}
+               task (assoc :task task))
+      "" (throw (ex-info "Action not found"
+                         {:some "data that helps debug problem, for example arguments that were passsed to the function"}))
+      (throw (ex-info "Incorrect action"
+                      {:some "data that helps debug problem, for example arguments that were passsed to the function"})))))
 
 (defn -main [action & args]
   (println (parse-args action args)))
 
+; ((str/lower-case action)
+ 
+;  (if (= action "")
+;    (throw (ex-info "Explain the error to the other developer. This is what will show up in the terminal, not to the user"
+;                    {:some "data that helps debug problem, for example arguments that were passsed to the function"})))
+;  )
