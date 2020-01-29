aac38cfe4c8d730e4b8057c5814741c2af7456c7
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-29 17:42:36 +0100

    Add uuid API to generate random files to test

--- test/youdo/db_test.clj
+++ test/youdo/db_test.clj
@@ -1,19 +1,27 @@
 (ns youdo.db-test
   (:require [youdo.db :as db]
             [clojure.test :refer [is deftest]]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clj-http.client :as client]))
 
 (def test-file-1 (io/resource "test/tasks-list.youdo"))
-(def test-file-2 "test/add-data.youdo")
 (def test-action "Laundry")
+
+(def endpoint "https://www.uuidgenerator.net/api/version1")
+(defn get-uuid [endpoint] (-> (client/get endpoint)
+                              :body))
+(def test-file-2 (str "test/" (get-uuid endpoint) ".youdo"))
+
 
 (deftest reading-file-content
   (is (= (db/read! test-file-1) 
          "Clean the kitchen\nWash dishes\nDo groceries")))
 
 (deftest saving-file-with-data
-  (db/save! test-file-2 test-action)
-  (is (= (slurp test-file-2) test-action))
-  (io/delete-file test-file-2))
+  (let [test-file-2 (str "test/" (get-uuid endpoint) ".youdo")]
+    (db/save! test-file-2 test-action)
+    (is (= (slurp test-file-2) test-action))
+    (io/delete-file test-file-2)))
+
 
 ; (deftest face-with-exeptions-when-folder-doesnt-exist)
