d6cf2b3372319f7e65b456feb3986e26ab9c41a5
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-30 15:59:51 +0100

    Refactore getting UUID without connecting to API

--- test/youdo/db_test.clj
+++ test/youdo/db_test.clj
@@ -1,16 +1,12 @@
 (ns youdo.db-test
   (:require [youdo.db :as db]
             [clojure.test :refer [is deftest]]
-            [clojure.java.io :as io]
-            [clj-http.client :as client]))
+            [clojure.java.io :as io]))
 
 (def test-file-1 (io/resource "test/tasks-list.youdo"))
 (def test-action "Laundry")
 
-(def endpoint "https://www.uuidgenerator.net/api/version1")
-(defn get-uuid [endpoint] (-> (client/get endpoint)
-                              :body))
-(def test-file-2 (str "test/" (get-uuid endpoint) ".youdo"))
+(def random-file-name (str "test/" (.toString (java.util.UUID/randomUUID)) ".youdo"))
 
 
 (deftest reading-file-content
@@ -18,7 +14,7 @@
          "Clean the kitchen\nWash dishes\nDo groceries")))
 
 (deftest saving-file-with-data
-  (let [test-file-2 (str "test/" (get-uuid endpoint) ".youdo")]
+  (let [test-file-2 random-file-name]
     (db/save! test-file-2 test-action)
     (is (= (slurp test-file-2) test-action))
     (io/delete-file test-file-2)))
