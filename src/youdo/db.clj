e680b6eadbe513ed59ceea2f1980b6a4c21916e4
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-31 12:11:28 +0100

    Fix typo

--- src/youdo/db.clj
+++ src/youdo/db.clj
@@ -3,11 +3,11 @@
 
 (defn save!
   "Given a path, replaces contents of the file with the given data. 
-   If the file doesn't exist it will be created under given path."
+   If the file doesn't exist it will be created under the given path."
   [path data]
   (spit path data))
 
-(defn read-content
-  "Given a path returns a content of the file."
+(defn read!
+  "Given a path returns the content of the file."
   [path]
   (slurp path))
