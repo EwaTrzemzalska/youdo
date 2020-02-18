91da39ea5cf8f193ac57727c1db3d8d95b1cbb99
Ewa Trzemżalska <trzemson@gmail.com>
2020-02-18 11:13:20 +0100

    Add separate function for creating new task

--- src/youdo/state.clj
+++ src/youdo/state.clj
@@ -6,8 +6,12 @@
   [path]
   (db/read-content path))
 
+(defn create-task 
+  [task-name]
+  {:done false :task-name task-name})
+
 (defn add-task
   "Given a file path, adds task to the end of the tasks list."
   [path task-name]
-  (let [task {:done false :task-name task-name}]
+  (let [task (create-task task-name)]
     (db/save! path (conj (db/read-content path) task))))
